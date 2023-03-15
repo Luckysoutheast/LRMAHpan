c7f90d2fa06434534acd8b38a852b18f286e401d
Luckysoutheast <106532415+Luckysoutheast@users.noreply.github.com>
2023-03-15 15:25:39 +0800

    push

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from config import Config
-from datetime import timedelta
-
-app = Flask(__name__)
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
-app.config.from_object(Config)
-
-from app import routes
